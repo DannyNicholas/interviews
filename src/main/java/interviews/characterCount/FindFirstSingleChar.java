07360b84382a14b404069c30f174c8d1c0d52f37
DannyNicholas <dannynicholas23@gmail.com>
2018-03-09 18:44:36 +0000

    add find first char tests

--- src/main/java/interviews/characterCount/FindFirstSingleChar.java
+++ src/main/java/interviews/characterCount/FindFirstSingleChar.java
@@ -10,38 +10,50 @@ public class FindFirstSingleChar {
     public static void main(String[] args) {
 
         FindFirstSingleChar sol = new FindFirstSingleChar();
-        sol.findFirstSingleChar("hello");
-        sol.findFirstSingleChar("zzyxxx");
-        sol.findFirstSingleChar("kayak");
+
+        if (sol.testCases()) {
+            System.out.println("All tests pass.");
+        }
+        else {
+            System.out.println("Test failed.");
+        }
     }
 
-    public void findFirstSingleChar(final String word) {
+    private boolean testCases() {
+
+        boolean first = (findFirstSingleChar("aabcdef") == 'b');
+        boolean second = (findFirstSingleChar("hello") == 'h');
+        boolean third = (findFirstSingleChar("zzyxxx") == 'y');
+        boolean fourth = (findFirstSingleChar("kayak") == 'y');
+
+        return first && second && third && fourth;
+    }
+
+
+    public char findFirstSingleChar(final String word) {
 
         System.out.println(word);
 
         char[] chars = word.toCharArray();
 
-        // this is flawed as we lose ordering with a hash-map
-        // we can no longer find the first char
         Map<Character, Integer> charCount = new HashMap<>();
 
         for(char c : chars) {
-            if (charCount.containsKey(c)) {
-                int count = charCount.get(c);
-                charCount.put(c, count + 1);
-            }
-            else {
-                charCount.put(c , 1);
-            }
+            int count = charCount.getOrDefault(c, 0);
+            charCount.put(c , count + 1);
         }
         System.out.println(charCount);
 
+        Character firstChar = null;
         for(char c : chars) {
             int count = charCount.get(c);
             if (count == 1) {
                 System.out.println(c + " - " + count);
+                firstChar = c;
                 break;
             }
         }
+
+        return firstChar;
     }
 }
